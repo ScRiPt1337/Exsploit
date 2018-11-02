8400e781a6ce76022655c91ecd03cece25f00b7d
ScRipt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-11-03 03:59:24 +0530

    Update hook.js

--- anon/hook.js
+++ anon/hook.js
@@ -1,4 +1,48 @@
 console.log("fucker is here");
 
 
-document.getElementsByTagName("form")[0].setAttribute("action", "jgolfdjglrdjlgdfgdf");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
